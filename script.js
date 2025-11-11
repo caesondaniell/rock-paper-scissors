b58b20195d423c468c18f32a0a48e972271d3e27
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2025-11-10 17:56:17 -0800

    Create repo and add files
    I forgot to create the repo. Whoopsie.

---
+++ script.js
@@ -0,0 +1,61 @@
+// console.log("Hello World!");
+
+function getComputerChoice() {
+    let choice = getRandomInt(3);
+    if (choice === 2) {
+        choice = "paper";
+    } else if (choice === 1) {
+        choice = "rock";
+    }
+    else choice = "scissors";
+    console.log(`Computer chooses ${choice}!`);
+}
+
+function getRandomInt(num) {
+    return Math.floor(Math.random() * num);
+}
+
+function getHumanChoice() {
+    let pick = prompt("rock, paper, or scissors?");
+    if (!(pick === "rock" ||
+        pick === "paper" ||
+        pick === "scissors"
+    )) {
+        console.log("You must enter 'paper' 'scissors' or 'rock'!");
+        return 0
+    } else {
+        console.log(`Player chooses ${pick}!`);
+        return pick.toLowerCase();
+    }
+}
+
+// getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (!humanChoice) {
+        console.log("Computer wins by default.");
+        computerScore++;
+    } else if (humanChoice === computerChoice) {
+        console.log("It's a tie! No points.");
+    } else if (
+        humanChoice === "rock" && computerChoice === "paper" ||
+        humanChoice === "scissors" && computerChoice === "rock" ||
+        humanChoice === "paper" && computerChoice === "scissors"
+    ) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    } else if (
+        humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper"
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    }
+    console.log(`Human ${humanScore}; Computer ${computerScore}.`);
+}
+
+playRound(getHumanChoice(), getComputerChoice());
