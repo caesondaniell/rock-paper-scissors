769950fbd29230d8d330c5a993020d10c5ce6744
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2025-11-20 17:12:14 -0800

    Add basic UI to RPS game
    Added basic UI with buttons to start the game,
    select rock, paper, or scissors, and reset the
    game after someone reaches 5 points.
    It's not pretty yet, but it does everything I
    want it to do.

--- script.js
+++ script.js
@@ -1,73 +1,79 @@
-// console.log("Hello World!");
-
 function getComputerChoice() {
-    let choice = getRandomInt(3);
+    let choice = getRandomInt();
     if (choice === 2) {
         choice = "paper";
     } else if (choice === 1) {
         choice = "rock";
     }
     else choice = "scissors";
-    console.log(`Computer chooses ${choice}!`);
     return choice;
 }
 
-function getRandomInt(num) {
-    return Math.floor(Math.random() * num);
+function getRandomInt() {
+    return Math.floor(Math.random() * 3);
 }
 
-function getHumanChoice() {
-    let pick = prompt("rock, paper, or scissors?");
-    if (!(pick.toLowerCase() === "rock" ||
-        pick.toLowerCase() === "paper" ||
-        pick.toLowerCase() === "scissors"
-    )) {
-        console.log("You must enter 'paper' 'scissors' or 'rock'!");
-        return 0
-    } else {
-        console.log(`Player chooses ${pick.toLowerCase()}!`);
-        return pick.toLowerCase();
+function playGame(humanChoice, computerChoice) {
+    const gameText = game.lastElementChild;
+    if (humanChoice === computerChoice) {
+        gameText.textContent = `You chose ${humanChoice} and the computer chose ${computerChoice}. It's a tie! No points.`;
+    } else if (
+        humanChoice === "rock" && computerChoice === "paper" ||
+        humanChoice === "scissors" && computerChoice === "rock" ||
+        humanChoice === "paper" && computerChoice === "scissors"
+    ) {
+        gameText.textContent = `You chose ${humanChoice} and the computer chose ${computerChoice}. You lose! ${computerChoice[0].toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.`;
+        computerScore++;
+        cScore.textContent = computerScore;
+    } else if (
+        humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper"
+    ) {
+        gameText.textContent = `You chose ${humanChoice} and the computer chose ${computerChoice}. You win! ${humanChoice[0].toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`;
+        humanScore++;
+        hScore.textContent = humanScore;
     }
-}
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(humanChoice, computerChoice) {
-        if (!humanChoice) {
-            console.log("Computer wins by default.");
-            computerScore++;
-        } else if (humanChoice === computerChoice) {
-            console.log("It's a tie! No points.");
-        } else if (
-            humanChoice === "rock" && computerChoice === "paper" ||
-            humanChoice === "scissors" && computerChoice === "rock" ||
-            humanChoice === "paper" && computerChoice === "scissors"
-        ) {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-            computerScore++;
-        } else if (
-            humanChoice === "rock" && computerChoice === "scissors" ||
-            humanChoice === "paper" && computerChoice === "rock" ||
-            humanChoice === "scissors" && computerChoice === "paper"
-        ) {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-            humanScore++;
-        } 
-        console.log(`Human ${humanScore} | Computer ${computerScore}`);
-    }
-
-    for (let i = 1; i <= 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (humanScore > computerScore) {
-        console.log("You win the game! Congratulations!");
-    } else if (humanScore < computerScore) {
-        console.log("You lose the game. Better luck next time!");
-    } else {
-        console.log("It's a tie game!");
+    if (humanScore === 5 || computerScore === 5) {
+        end.firstElementChild.textContent = humanScore > computerScore ?
+        `Game Over! Computer Score: ${computerScore} | Player Score: ${humanScore} You win! Congratulations!` :
+        `Game Over! Computer Score: ${computerScore} | Player Score: ${humanScore} You lose! Better luck next time.`;
+        end.classList.toggle("hidden");
+        game.classList.toggle("hidden");
+        hScore.textContent = 0;
+        cScore.textContent = 0;
     }
 }
 
-playGame();
+const top = document.querySelector(".top");
+const game = document.querySelector(".gameContent");
+const end = document.querySelector(".endMessage");
+const btns = document.querySelectorAll("button");
+const hScore = document.querySelector(".hScore");
+const cScore = document.querySelector(".cScore");
+let humanScore = 0;
+let computerScore = 0;
+console.log(btns);
+btns.forEach((button) => {
+    button.addEventListener("click", () => {
+        switch (button.textContent) {
+            case "START THE GAME":
+                top.classList.toggle("hidden");
+                game.classList.toggle("hidden");
+                break;
+            case "ROCK":
+                playGame(button.textContent.toLowerCase(), getComputerChoice());
+                break;
+            case "PAPER":
+                playGame(button.textContent.toLowerCase(), getComputerChoice());
+                break;
+            case "SCISSORS":
+                playGame(button.textContent.toLowerCase(), getComputerChoice());
+                break;
+            case "RESET":
+                top.classList.toggle("hidden");
+                end.classList.toggle("hidden");
+                break;
+        }
+    })
+})
